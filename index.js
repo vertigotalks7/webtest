40e6efbcbf2abf8077bb3969223d4858eb58abcc
Angelo <100081282+vertigotalks7@users.noreply.github.com>
2025-08-26 13:56:58 +0530

    COMPLETED (1)

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const decreasebtn = Document.getElementById("decbtn");
-const reset = Document.getElementById("re");
-const increasebtn = Document.getElementById("incbtn");
+const decreasebtn = document.getElementById("decbtn");
+const reset = document.getElementById("re");
+const increasebtn = document.getElementById("incbtn");
 
-const countlabel = Document.getElementById("countlabel");
+const countlabel = document.getElementById("countlabel");
 let count= 0;
 
 increasebtn.onclick = function(){
   count++;
+  countlabel.textContent = count;
+}
+
+decreasebtn.onclick = function(){
+  count--;
+  countlabel.textContent = count;
+}
+
+reset.onclick = function(){
+  count = 0;
   countlabel.textContent = count;
 }
